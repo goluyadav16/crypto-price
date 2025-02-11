d7cab7734cf9a23436cb1ec1cda854ebcfff385a
goluyadav16 <goluyadav1676@gmail.com>
2025-02-11 12:59:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PriceTracker from './PriceTracker'; // Import the PriceTracker component
+
+function App() {
+  return (
+    <div className="App">
+      <PriceTracker /> {/* Use the PriceTracker component */}
+    </div>
+  );
+}
+
+export default App;
