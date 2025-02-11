695ed21323f142bb9ffd520fec4b496aa8025d66
goluyadav16 <goluyadav1676@gmail.com>
2025-02-11 13:01:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // This line is important
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
+
+// Rest of the code remains same
+
+// Rest of the code remains same//
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';  // Path सही होना चाहिए
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
